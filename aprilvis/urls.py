64cba5bd3898d72d68b8abc0babf8461f90821fe
Maikel <Maikel.withagen@gmail.com>
2015-05-22 10:35:23 +0200

    First upload

---
+++ aprilvis/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+
+urlpatterns = patterns('',
+    # Root URL
+    url(r'^/?$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^circles.html$', TemplateView.as_view(template_name='layout/circles.html'), name='circles'),
+    url(r'^heatmap.html$', TemplateView.as_view(template_name='layout/heatmap.html'), name='heatmap'),
+    url(r'^favicon.ico/$', lambda x: HttpResponseRedirect('static/img/favicon.ico')),
+)
