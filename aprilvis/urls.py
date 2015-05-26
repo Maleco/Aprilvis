70233b13493f64f4e1a38cd176b6a0e440a6a248
Maikel <Maikel.withagen@gmail.com>
2015-05-26 15:48:23 +0200

    Second upload

--- aprilvis/urls.py
+++ aprilvis/urls.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect
 urlpatterns = patterns('',
     # Root URL
     url(r'^/?$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^/results', TemplateView.as_view(template_name='results.html'), name='results'),
     url(r'^circles.html$', TemplateView.as_view(template_name='layout/circles.html'), name='circles'),
     url(r'^heatmap.html$', TemplateView.as_view(template_name='layout/heatmap.html'), name='heatmap'),
     url(r'^favicon.ico/$', lambda x: HttpResponseRedirect('static/img/favicon.ico')),
