68b1f0eea348be57eb932db3c9e35ae18c00164b
Maikel (DirectpVPS) <maikel.withagen@gmail.com>
2015-06-09 22:03:10 +0000

    Updated graphics

--- results.php
+++ results.php
@@ -21,7 +21,6 @@
 			<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
 			<!-- Custom CSS -->
-			<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
 			<link href="css/stylish-portfolio.css" rel="stylesheet" />
 			<link href="css/style.css" rel="stylesheet" />
 
@@ -35,101 +34,128 @@
 
 			<!-- JQuery -->
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+			<script src="js/jquery.csv-0.71.min.js"></script>
 			<!-- Google Maps API -->
 			<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
 			<script src="js/maps.js"></script>
-			<!-- FullPage -->
-			<script type="text/javascript" src="js/jquery.fullPage.min.js"></script>
-			<script type="text/javascript" src="js/jquery.slimscroll.min.js"></script>
 
 			<!-- Bootstrap -->
 			<link rel="stylesheet" href="css/bootstrap.min.css" />
 			<script type="text/javascript" src="js/bootstrap.min.js"></script>
-			<!-- Papaparse -->
-			<script type="text/javascript" src="js/jquery.csv-0.71.min.js"></script>
 
+			<!-- D3 -->
+			<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.4/d3.min.js"></script>
+			<!-- D3pie -->
+			<script src="js/d3pie.min.js"></script>
+
+			<!-- Load c3 -->
+			<link href="css/c3.min.css" rel="stylesheet" type="text/css">
+			<script src="js/c3.min.js"></script>
 	 </head>
 
 	 <body>
-			<div id="header">
-				 <div id="logo_area">
-						<a href="index.html"><img alt="Brand" src="img/logo_clear.png"></a>
-				 </div>
-			</div>
-
-			<div id="fullpage">
-				 <!-- Main -->
-				 <div id="main" class="section">
-						<div class="content">
-							 <div style="display:none" id="nodeValue"><?php echo htmlspecialchars($_GET["node"])?></div>
-							 <!-- Top Row -->
-							 <div class="row" style="padding-top:20px;">
-									<!-- Node Name + Info -->
-									<div class="col-md-3">
-										 <div class="panel panel-default">
-												<div id="nodeName" class="panel-heading">
-													 <!-- Single button -->
-													 <div id="nodeDropDown" class="dropdown">
-													 </div>
-												</div>
-												<div id="nodeDescription" class="panel-body">
-												</div>
-										 </div>
-									</div>
-									<!-- Node Google Map -->
-									<div class="col-md-4">
-										 <div class="panel panel-default">
-												<div class="panel-heading">Google Maps location</div>
-												<div class="panel-body">
-													 <div id="smallmap-canvas"></div>
-												</div>
-										 </div>
-									</div>
-									<!-- Node Appraisal Plot -->
-									<div class="col-md-5">
-										 <div class="panel panel-default">
-												<div id="nodeName" class="panel-heading">
-													 <!-- Select between human and system appraisal -->
-													 <div class="btn-group" data-toggle="buttons">
-															<label id="humanAppraisal" class="btn btn-primary">
-																 <input type="radio" name="options" id="option2"> Human Appraisal
-															</label>
-															<label id="machineAppraisal" class="btn btn-primary">
-																 <input type="radio" name="options" id="option3"> Machine Appraisal
-															</label>
-													 </div>
-												</div>
-												<div id="nodeAppraisalPlot" class="panel-body"></div>
-										 </div>
-
-									</div> 
-							 </div>
-							 <div class="row">
-							 </div>
+			<div id="container">
+				 <div id="header">
+						<div id="logo_area">
+							 <a href="index.html"><img alt="Brand" src="img/logo_clear.png"></a>
 						</div>
 				 </div>
+
+				 <!-- Node id via URl -->
+				 <div style="display:none" id="nodeValue"><?php echo htmlspecialchars($_GET["node"])?></div>
+
+				 <!-- Main -->
+				 <div id="main" class="content" style="margin-top:60px">
+						<!-- Top Row -->
+						<div class="row" style="padding-top:20px;">
+							 <!-- Node Name + Info + Google Map -->
+							 <div class="col-md-4">
+									<div class="panel panel-default">
+										 <div id="nodeName" class="panel-heading">
+												<!-- Single button -->
+												<div id="nodeDropDown" class="dropdown">
+												</div>
+										 </div>
+										 <div id="nodeDescription" class="panel-body">
+										 </div>
+									</div>
+							 </div>
+							 <!-- Node Name + Info + Google Map -->
+
+							 <!-- Node Appraisal Plot -->
+							 <div class="col-md-4">
+									<div class="panel panel-default">
+										 <div id="nodeName" class="panel-heading">
+												<!-- Select between human and system appraisal -->
+												<div class="btn-group" data-toggle="buttons">
+													 <label id="humanAppraisal" class="btn btn-info">
+															<input type="radio" name="options" id="option2"> Human Appraisal
+													 </label>
+													 <label id="machineAppraisal" class="btn btn-info">
+															<input type="radio" name="options" id="option3"> Machine Appraisal
+													 </label>
+												</div>
+										 </div>
+										 <div id="nodeAppraisalPlot" class="panel-body"></div>
+									</div>
+							 </div> 
+							 <!-- Node Appraisal Plot -->
+
+							 <!-- Pie Chart -->
+							 <div class="col-md-4">
+									<div class="panel panel-default" style="height:auto;">
+										 <div class="panel-heading">
+												<button type="button" class="btn btn-info btn-block">
+													 Pie Chart
+												</button>
+										 </div>
+									</div>
+									<div id="pieChart"></div>
+							 </div>
+							 <!-- Pie Chart -->
+						</div> 
+						<!-- Top Row -->
+
+						<!-- Second Row -->
+						<div class="row" style="padding-top:20px;">
+							 <div class="col-md-12">
+									<div class="panel panel-default">
+										 <div class="panel-heading">
+												<button type="button" class="btn btn-info btn-block">
+													 Line Graph (Features)</div>
+										 </button>
+										 <div class="panel-body">
+												<div id="chart"></div>
+										 </label>
+									</div>
+							 </div>
+						</div> 
+						<!-- Second Row -->
+				 </div>
+				 <!-- Main -->
 			</div>
+	 </div>
 
-			<script>
-				 $(document).ready(function() {
-							 $('#fullpage').fullpage({
-										 paddingTop: 120,
-							 });
+	 <script src="js/results.js"></script>
+	 <script>
+			$(document).ready(function() {
+						// Initialize the Google Map
 
-							 initializeOne(node);
-							 $('#humanAppraisal').on('click', function (e) {
-										 document.getElementById("nodeAppraisalPlot").innerHTML = 
-										 '<img alt="Human Appraisal Plot" src="img/human_appraisals/Human_Appraisal_node' 
-										 + zeroPad(node,3) + '.png">';
-							 });
-							 $('#machineAppraisal').on('click', function (e) {
-										 document.getElementById("nodeAppraisalPlot").innerHTML = 
-										 '<img alt="Human Appraisal Plot" src="img/appraisals/ESN_Appraisal_Density_node' + 
-										 zeroPad(node,3) + '.png">';
-							 });
-				 });
+						// Set up the Appraisal toggle buttons
+						$('#humanAppraisal').on('click', function (e) {
+									document.getElementById("nodeAppraisalPlot").innerHTML = 
+									'<img alt="Human Appraisal Plot" src="img/human_appraisals/Human_Appraisal_node' 
+									+ zeroPad(node,3) + '.png">';
+						});
+						$('#machineAppraisal').on('click', function (e) {
+									document.getElementById("nodeAppraisalPlot").innerHTML = 
+									'<img alt="Human Appraisal Plot" src="img/appraisals/ESN_Appraisal_Density_node' + 
+									zeroPad(node,3) + '.png">';
+						});
 
-			</script>
-			<script src="js/results.js"></script>
-	 </body>
+
+			});
+
+	 </script>
+</body>
 </html>
