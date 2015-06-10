85c0a4eb544083fc322b36f479ad1720cea398b5
Maikel (DirectpVPS) <maikel.withagen@gmail.com>
2015-06-10 10:57:58 +0000

    Added data to all nodes/Updated graphs

--- js/results.js
+++ js/results.js
@@ -1,243 +1,244 @@
 // Function to add leading zeros
 
 function zeroPad(num, numZeros) {
-	 var n = Math.abs(num);
-	 var zeros = Math.max(0, numZeros - Math.floor(n).toString().length );
-	 var zeroString = Math.pow(10,zeros).toString().substr(1);
-	 if( num < 0 ) {
-			zeroString = '-' + zeroString;
-	 }
+   var n = Math.abs(num);
+   var zeros = Math.max(0, numZeros - Math.floor(n).toString().length );
+   var zeroString = Math.pow(10,zeros).toString().substr(1);
+   if( num < 0 ) {
+      zeroString = '-' + zeroString;
+   }
 
-	 return zeroString+n;
+   return zeroString+n;
 }
 var node = document.getElementById("nodeValue").innerHTML;
 var nodeData = citymap[node];
 
 // Read in a csv for this node
 $.ajax({
-	 url: "data/data_concat/node" + zeroPad(node,3) +  ".csv",
-	 success: function (csvd) {
-			data = $.csv2Array(csvd);
-	 },
-	 dataType: "text",
-	 complete: function () {
-			// call a function on complete 
-			doStuff(data);
-	 }
+   url: "data/data_concat/node" + zeroPad(node,3) +  ".csv",
+   success: function (csvd) {
+      data = $.csv2Array(csvd);
+   },
+   dataType: "text",
+   complete: function () {
+      // call a function on complete 
+      doStuff(data);
+   }
 });
 
 function doStuff (data) {
 
-	 document.getElementById("nodeDescription").innerHTML = 
-	 nodeData.description +
-			'<br><br><div id="smallmap-canvas"></div>';
-	 initializeOne(node);
+   document.getElementById("nodeDescription").innerHTML = 
+      nodeData.description +
+      '<br><br><div id="smallmap-canvas"></div>';
+   initializeOne(node);
 
-	 // Toggle menu for the appraisals
-	 $("#humanAppraisal").button('toggle');
-	 document.getElementById("nodeAppraisalPlot").innerHTML = 
-	 '<img alt="Human Appraisal Plot" src="img/human_appraisals/Human_Appraisal_node' 
-	 + zeroPad(node,3) + '.png">';
+   // Toggle menu for the appraisals
+   $("#humanAppraisal").button('toggle');
+   document.getElementById("nodeAppraisalPlot").innerHTML = 
+      '<img alt="Human Appraisal Plot" src="img/human_appraisals/Human_Appraisal_node' 
+      + zeroPad(node,3) + '.png">';
 
-	 // Create a nice dropdown menu to switch nodes 
-	 var dropdownHTML = '<button type="button" class="btn btn-info btn-block dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="color:white">' +
-			'Location ' + node + ": " + nodeData.title + '    <span class="caret"></span></button>' +
-			'<ul class="dropdown-menu" role="menu">' +
-			'<li><a href="results.php?node=' + node + '">Location ' + node + ": " + nodeData.title + '    </a></li>' +
-			'<li class="divider"></li>';
+   // Create a nice dropdown menu to switch nodes 
+   var dropdownHTML = '<button type="button" class="btn btn-info btn-block dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="color:white">' +
+      'Location ' + node + ": " + nodeData.title + '    <span class="caret"></span></button>' +
+      '<ul class="dropdown-menu" role="menu">' +
+      '<li><a href="results.php?node=' + node + '">Location ' + node + ": " + nodeData.title + '    </a></li>' +
+      '<li class="divider"></li>';
 
-	 for (var nod in citymap) 
-	 {
-			dropdownHTML += 
-			'<li><a href="results.php?node=' + nod + '">Location ' + nod + ": " + 
-				 citymap[nod].title + '    </a></li>';
-	 }
+   for (var nod in citymap) 
+   {
+      dropdownHTML += 
+         '<li><a href="results.php?node=' + nod + '">Location ' + nod + ": " + 
+         citymap[nod].title + '    </a></li>';
+   }
 
-	 dropdownHTML += '</ul>';
-	 document.getElementById("nodeDropDown").innerHTML = dropdownHTML;
+   dropdownHTML += '</ul>';
+   document.getElementById("nodeDropDown").innerHTML = dropdownHTML;
 
-	 // And now the csv graphs
+   // And now the csv graphs
 
-	 // First gather the data
-	 
-	 // Sum the 4 quarters (proportion of the total)
-	 //  top left, top right, bot left, bot right
-	 var pleasantness = [];
-	 var eventfulness = [];
-	 var foreground = [];
-	 var birds = [];
-	 var speech = [];
-	 var child = [];
-	 var bike = [];
-	 var scooter = [];
-	 var car = [];
-	 var heavy = [];
+   // First gather the data
 
-	 var chaotic = 0;
-	 var lively = 0;
-	 var boring = 0;
-	 var calm = 0;
+   // Sum the 4 quarters (proportion of the total)
+   //  top left, top right, bot left, bot right
+   var pleasantness = [];
+   var eventfulness = [];
+   var foreground = [];
+   var birds = [];
+   var speech = [];
+   var child = [];
+   var bike = [];
+   var scooter = [];
+   var car = [];
+   var heavy = [];
 
-	 for (var i = 1; i < data.length; i++) {
-			pleasantness.push(data[i][0]);
-			eventfulness.push(data[i][1]);
-			foreground.push(data[i][2]);
-			birds.push(data[i][3]);
-			speech.push(data[i][4]);
-			child.push(data[i][5]);
-			bike.push(data[i][6]);
-			scooter.push(data[i][7]);
-			car.push(data[i][8]);
-			heavy.push(data[i][9]);
+   var chaotic = 0;
+   var lively = 0;
+   var boring = 0;
+   var calm = 0;
 
-			if (data[i][0] < 0 & data[i][1] < 0) chaotic++;	
-			if (data[i][0] > 0 & data[i][1] < 0) lively++;	
-			if (data[i][0] < 0 & data[i][1] > 0) boring++;	
-			if (data[i][0] > 0 & data[i][1] > 0) calm++;	
-	 }
+   for (var i = 1; i < data.length; i++) {
+      pleasantness.push(data[i][0]);
+      eventfulness.push(data[i][1]);
+      foreground.push(data[i][2]);
+      birds.push(data[i][3]);
+      speech.push(data[i][4]);
+      child.push(data[i][5]);
+      bike.push(data[i][6]);
+      scooter.push(data[i][7]);
+      car.push(data[i][8]);
+      heavy.push(data[i][9]);
 
-	 function averagePerSecond (input) {
-			var output = [];
-			for (var i = 0; i < input.length; i+=10) {
-				 output.push( 
-						Number(input[i]  ) + 
-							 Number(input[i+1]) + 
-							 Number(input[i+2]) + 
-							 Number(input[i+3]) + 
-							 Number(input[i+4]) + 
-							 Number(input[i+5]) + 
-							 Number(input[i+6]) + 
-							 Number(input[i+7]) + 
-							 Number(input[i+8]) + 
-							 Number(input[i+9]) ) ; 
-			}
-			return output;
-	 }
+      if (data[i][0] < 0 & data[i][1] < 0) chaotic++;	
+      if (data[i][0] > 0 & data[i][1] < 0) lively++;	
+      if (data[i][0] < 0 & data[i][1] > 0) boring++;	
+      if (data[i][0] > 0 & data[i][1] > 0) calm++;	
+   }
 
-	 var avgChild = []
-	 //Average over the seconds
-	 for (var i = 0; i < data.length; i+=10) {
-			avgChild.push( 
-				 Number(child[i]  ) + 
-				 Number(child[i+1]) + 
-				 Number(child[i+2]) + 
-				 Number(child[i+3]) + 
-				 Number(child[i+4]) + 
-				 Number(child[i+5]) + 
-				 Number(child[i+6]) + 
-				 Number(child[i+7]) + 
-				 Number(child[i+8]) + 
-				 Number(child[i+9]) ) ; 
-	 }
+   function averagePerSecond (input) {
+      var output = [];
+      for (var i = 0; i < input.length; i+=10) {
+         output.push( 
+               Number(input[i]  ) + 
+               Number(input[i+1]) + 
+               Number(input[i+2]) + 
+               Number(input[i+3]) + 
+               Number(input[i+4]) + 
+               Number(input[i+5]) + 
+               Number(input[i+6]) + 
+               Number(input[i+7]) + 
+               Number(input[i+8]) + 
+               Number(input[i+9]) ) ; 
+      }
+      return output;
+   }
 
-	 avgFG = averagePerSecond(averagePerSecond(foreground));
-	 avgFG.unshift("Foreground");
-	 avgBirds = averagePerSecond(averagePerSecond(birds));
-	 avgBirds.unshift("Birds");
-	 avgSpeech = averagePerSecond(averagePerSecond(speech));
-	 avgSpeech.unshift("Speech");
-	 avgChild = averagePerSecond(averagePerSecond(child));
-	 avgChild.unshift("Child");
-	 avgBike = averagePerSecond(averagePerSecond(bike));
-	 avgBike.unshift("Bike");
-	 avgScooter= averagePerSecond(averagePerSecond(scooter));
-	 avgScooter.unshift("Scooter");
-	 avgCar = averagePerSecond(averagePerSecond(car));
-	 avgCar.unshift("Car");
-	 avgHeavy = averagePerSecond(averagePerSecond(heavy));
-	 avgHeavy.unshift("Heavy");
+   var avgChild = []
+      //Average over the seconds
 
-	 var chart = c3.generate({
-			bindto: '#chart',
-			data: {
-				 columns: [avgFG, avgBirds, avgSpeech, avgChild, avgBike, avgScooter, avgCar, avgHeavy]
-			}
-	 });
+//      avgFG = averagePerSecond(averagePerSecond(foreground));
+//   avgFG.unshift("Foreground");
+   avgBirds = averagePerSecond(averagePerSecond(birds));
+   avgBirds.unshift("Birds");
+   avgSpeech = averagePerSecond(averagePerSecond(speech));
+   avgSpeech.unshift("Speech");
+//   avgChild = averagePerSecond(averagePerSecond(child));
+//   avgChild.unshift("Child");
+//   avgBike = averagePerSecond(averagePerSecond(bike));
+//   avgBike.unshift("Bike");
+//   avgScooter= averagePerSecond(averagePerSecond(scooter));
+//   avgScooter.unshift("Scooter");
+   avgCar = averagePerSecond(averagePerSecond(car));
+   avgCar.unshift("Car");
+//   avgHeavy = averagePerSecond(averagePerSecond(heavy));
+//   avgHeavy.unshift("Heavy");
 
-	 // Now generate the pie chart
-	 var pie = new d3pie("pieChart", {
-			"header": {
-				 "title": {
-						"fontSize": 25,
-						"font": "open sans"
-				 },
-				 "subtitle": {
-						"color": "#999999",
-						"fontSize": 12,
-						"font": "open sans"
-				 },
-				 "location": "top-left",
-				 "titleSubtitlePadding": 9
-			},
-			"size": {
-				 "canvasWidth": 350,
-				 "pieOuterRadius": "70%"
-			},
-			"data": {
-				 "sortOrder": "value-asc",
-				 "content": [
-						{
-							 "label": "Chaotic",
-							 "value": chaotic,
-							 "color": "#e700d2"
-						},
-						{
-							 "label": "Calm",
-							 "value": calm,
-							 "color": "#228835"
-						},
-						{
-							 "label": "Lively",
-							 "value": lively,
-							 "color": "#d9e98d"
-						},
-						{
-							 "label": "Boring",
-							 "value": boring,
-							 "color": "#ff0000"
-						}
-				 ]
-			},
-			"labels": {
-				 "outer": {
-						"format": "label-percentage2",
-						"pieDistance": 22
-				 },
-				 "inner": {
-						"format": "none",
-						"hideWhenLessThanPercentage": 3
-				 },
-				 "mainLabel": {
-						"fontSize": 15
-				 },
-				 "percentage": {
-						"color": "#ffffff",
-						"fontSize": 15,
-						"decimalPlaces": 0
-				 },
-				 "value": {
-						"color": "#ffffff",
-						"fontSize": 25
-				 },
-				 "lines": {
-						"enabled": true
-				 },
-				 "truncation": {
-						"enabled": true
-				 }
-			},
-			"effects": {
-				 "pullOutSegmentOnClick": {
-						"speed": 400,
-						"size": 10
-				 }
-			},
-			"misc": {
-				 "gradient": {
-						"enabled": true,
-						"percentage": 100
-				 }
-			}
-	 });
+   var chart = c3.generate({
+      bindto: '#chart',
+       data: {
+          columns: [avgBirds, avgSpeech, avgCar]
+       },
+   });
+
+   var chart = c3.generate({
+      bindto: "#pieChart",
+       data: {
+          // iris data from R
+          columns: [
+      ['Chaotic', chaotic],
+       ['Lively' , lively],
+       ['Boring' , boring],
+       ['Calm'   , calm],
+       ],
+       type : 'pie',
+       }
+   });
+
+   // Now generate the pie chart
+   //	 var pie = new d3pie("pieChart", {
+   //			"header": {
+   //				 "title": {
+   //						"fontSize": 25,
+   //						"font": "open sans"
+   //				 },
+   //				 "subtitle": {
+   //						"color": "#999999",
+   //						"fontSize": 12,
+   //						"font": "open sans"
+   //				 },
+   //				 "location": "top-left",
+   //				 "titleSubtitlePadding": 9
+   //			},
+   //			"size": {
+   //				 "canvasWidth": 400,
+   //				 "pieOuterRadius": "70%"
+   //			},
+   //			"data": {
+   //				 "sortOrder": "value-asc",
+   //				 "content": [
+   //						{
+   //							 "label": "Chaotic",
+   //							 "value": chaotic,
+   //							 "color": "#ff0000"
+   //						},
+   //						{
+   //							 "label": "Calm",
+   //							 "value": calm,
+   //							 "color": "#00ff00"
+   //						},
+   //						{
+   //							 "label": "Lively",
+   //							 "value": lively,
+   //							 "color": "#ffff00"
+   //						},
+   //						{
+   //							 "label": "Boring",
+   //							 "value": boring,
+   //							 "color": "#0000ff"
+   //						}
+   //				 ]
+   //			},
+   //			"labels": {
+   //				 "outer": {
+   //						"format": "label-percentage2",
+   //						"pieDistance": 22
+   //				 },
+   //				 "inner": {
+   //						"format": "none",
+   //						"hideWhenLessThanPercentage": 3
+   //				 },
+   //				 "mainLabel": {
+   //						"fontSize": 15
+   //				 },
+   //				 "percentage": {
+   //						"color": "#ffffff",
+   //						"fontSize": 15,
+   //						"decimalPlaces": 0
+   //				 },
+   //				 "value": {
+   //						"color": "#ffffff",
+   //						"fontSize": 25
+   //				 },
+   //				 "lines": {
+   //						"enabled": true
+   //				 },
+   //				 "truncation": {
+   //						"enabled": true
+   //				 }
+   //			},
+   //			"effects": {
+   //				 "pullOutSegmentOnClick": {
+   //						"speed": 400,
+   //						"size": 10
+   //				 }
+   //			},
+   //			"misc": {
+   //				 "gradient": {
+   //						"enabled": true,
+   //						"percentage": 100
+   //				 }
+   //			}
+   //	 });
 }
 
--- results.php
+++ results.php
@@ -1,161 +1,179 @@
 <!DOCTYPE html>
 <?php 
-	 $node = $_GET["node"];
+   $node = $_GET["node"];
 ?>
 <html lang="en">
 
-	 <head>
+   <head>
 
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<meta name="description" content="">
-			<meta name="author" content="">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
 
-			<title>Aprilvis</title>
-			<link rel="shortcut icon" href="img/logo_clear.png">
-
-
-			<!-- Custom Fonts -->
-			<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-			<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-			<!-- Custom CSS -->
-			<link href="css/stylish-portfolio.css" rel="stylesheet" />
-			<link href="css/style.css" rel="stylesheet" />
+      <title>Aprilvis</title>
+      <link rel="shortcut icon" href="img/logo_clear.png">
 
 
-			<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-			<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-			<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-			<![endif]-->
+      <!-- Custom Fonts -->
+      <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+      <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-			<!-- JQuery -->
-			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-			<script src="js/jquery.csv-0.71.min.js"></script>
-			<!-- Google Maps API -->
-			<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
-			<script src="js/maps.js"></script>
-
-			<!-- Bootstrap -->
-			<link rel="stylesheet" href="css/bootstrap.min.css" />
-			<script type="text/javascript" src="js/bootstrap.min.js"></script>
-
-			<!-- D3 -->
-			<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.4/d3.min.js"></script>
-			<!-- D3pie -->
-			<script src="js/d3pie.min.js"></script>
-
-			<!-- Load c3 -->
-			<link href="css/c3.min.css" rel="stylesheet" type="text/css">
-			<script src="js/c3.min.js"></script>
-	 </head>
-
-	 <body>
-			<div id="container">
-				 <div id="header">
-						<div id="logo_area">
-							 <a href="index.html"><img alt="Brand" src="img/logo_clear.png"></a>
-						</div>
-				 </div>
-
-				 <!-- Node id via URl -->
-				 <div style="display:none" id="nodeValue"><?php echo htmlspecialchars($_GET["node"])?></div>
-
-				 <!-- Main -->
-				 <div id="main" class="content" style="margin-top:60px">
-						<!-- Top Row -->
-						<div class="row" style="padding-top:20px;">
-							 <!-- Node Name + Info + Google Map -->
-							 <div class="col-md-4">
-									<div class="panel panel-default">
-										 <div id="nodeName" class="panel-heading">
-												<!-- Single button -->
-												<div id="nodeDropDown" class="dropdown">
-												</div>
-										 </div>
-										 <div id="nodeDescription" class="panel-body">
-										 </div>
-									</div>
-							 </div>
-							 <!-- Node Name + Info + Google Map -->
-
-							 <!-- Node Appraisal Plot -->
-							 <div class="col-md-4">
-									<div class="panel panel-default">
-										 <div id="nodeName" class="panel-heading">
-												<!-- Select between human and system appraisal -->
-												<div class="btn-group" data-toggle="buttons">
-													 <label id="humanAppraisal" class="btn btn-info">
-															<input type="radio" name="options" id="option2"> Human Appraisal
-													 </label>
-													 <label id="machineAppraisal" class="btn btn-info">
-															<input type="radio" name="options" id="option3"> Machine Appraisal
-													 </label>
-												</div>
-										 </div>
-										 <div id="nodeAppraisalPlot" class="panel-body"></div>
-									</div>
-							 </div> 
-							 <!-- Node Appraisal Plot -->
-
-							 <!-- Pie Chart -->
-							 <div class="col-md-4">
-									<div class="panel panel-default" style="height:auto;">
-										 <div class="panel-heading">
-												<button type="button" class="btn btn-info btn-block">
-													 Pie Chart
-												</button>
-										 </div>
-									</div>
-									<div id="pieChart"></div>
-							 </div>
-							 <!-- Pie Chart -->
-						</div> 
-						<!-- Top Row -->
-
-						<!-- Second Row -->
-						<div class="row" style="padding-top:20px;">
-							 <div class="col-md-12">
-									<div class="panel panel-default">
-										 <div class="panel-heading">
-												<button type="button" class="btn btn-info btn-block">
-													 Line Graph (Features)</div>
-										 </button>
-										 <div class="panel-body">
-												<div id="chart"></div>
-										 </label>
-									</div>
-							 </div>
-						</div> 
-						<!-- Second Row -->
-				 </div>
-				 <!-- Main -->
-			</div>
-	 </div>
-
-	 <script src="js/results.js"></script>
-	 <script>
-			$(document).ready(function() {
-						// Initialize the Google Map
-
-						// Set up the Appraisal toggle buttons
-						$('#humanAppraisal').on('click', function (e) {
-									document.getElementById("nodeAppraisalPlot").innerHTML = 
-									'<img alt="Human Appraisal Plot" src="img/human_appraisals/Human_Appraisal_node' 
-									+ zeroPad(node,3) + '.png">';
-						});
-						$('#machineAppraisal').on('click', function (e) {
-									document.getElementById("nodeAppraisalPlot").innerHTML = 
-									'<img alt="Human Appraisal Plot" src="img/appraisals/ESN_Appraisal_Density_node' + 
-									zeroPad(node,3) + '.png">';
-						});
+      <!-- Custom CSS -->
+      <link href="css/stylish-portfolio.css" rel="stylesheet" />
+      <link href="css/style.css" rel="stylesheet" />
 
 
-			});
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
 
-	 </script>
+      <!-- JQuery -->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="js/jquery.csv-0.71.min.js"></script>
+      <!-- Google Maps API -->
+      <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+      <script src="js/maps.js"></script>
+
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+      <!-- D3 -->
+      <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.4/d3.min.js"></script>
+      <!-- D3pie -->
+      <script src="js/d3pie.min.js"></script>
+
+      <!-- Load c3 -->
+      <link href="css/c3.min.css" rel="stylesheet" type="text/css">
+      <script src="js/c3.min.js"></script>
+   </head>
+
+   <body>
+      <div id="container">
+         <div id="header">
+            <div id="logo_area">
+               <a href="index.html"><img alt="Brand" src="img/logo_clear.png"></a>
+            </div>
+         </div>
+
+         <!-- Node id via URl -->
+         <div style="display:none" id="nodeValue"><?php echo htmlspecialchars($_GET["node"])?></div>
+
+         <!-- Main -->
+         <div id="main" class="content" style="margin-top:60px">
+            <!-- Top Row -->
+            <div class="row" style="padding-top:20px;">
+               <!-- Node Name + Info + Google Map -->
+               <div class="col-md-4">
+                  <div class="panel panel-default">
+                     <div id="nodeName" class="panel-heading">
+                        <!-- Single button -->
+                        <div id="nodeDropDown" class="dropdown">
+                        </div>
+                     </div>
+                     <div id="nodeDescription" class="panel-body">
+                     </div>
+                  </div>
+               </div>
+               <!-- Node Name + Info + Google Map -->
+
+               <!-- Node Appraisal Plot -->
+               <div class="col-md-4">
+                  <div class="panel panel-default">
+                     <div id="nodeName" class="panel-heading">
+                        <!-- Select between human and system appraisal -->
+                        <div class="btn-group" data-toggle="buttons">
+                           <label id="humanAppraisal" class="btn btn-info">
+                              <input type="radio" name="options" id="option2"> Human Appraisal
+                           </label>
+                           <label id="machineAppraisal" class="btn btn-info">
+                              <input type="radio" name="options" id="option3"> Machine Appraisal
+                           </label>
+                        </div>
+                     </div>
+                     <div id="nodeAppraisalPlot" class="panel-body"></div>
+                  </div>
+               </div> 
+               <!-- Node Appraisal Plot -->
+
+               <!-- Pie Chart -->
+               <div class="col-md-4">
+                  <div class="panel panel-default" style="height:auto;">
+                     <div class="panel-heading">
+                        <button type="button" class="btn btn-info btn-block">
+                           Pie Chart
+                        </button>
+                     </div>
+                     <div class="panel-body">
+                        <div id="pieChart"></div>
+                        <button onclick="changeSomething();" class="btn btn-info">Knopje</button>
+                     </div>
+                  </div>
+               </div>
+               <!-- Pie Chart -->
+            </div> 
+            <!-- Top Row -->
+
+            <!-- Second Row -->
+            <div class="row" style="padding-top:20px;">
+               <div class="col-md-12">
+                  <div class="panel panel-default">
+                     <div class="panel-heading">
+                        <button type="button" class="btn btn-info btn-block">
+                           Line Graph (Features)</div>
+                     </button>
+                     <div class="panel-body">
+                        <div id="chart"></div>
+                     </label>
+                  </div>
+               </div>
+            </div> 
+            <!-- Second Row -->
+         </div>
+         <!-- Main -->
+      </div>
+   </div>
+
+   <script src="js/results.js"></script>
+   <script>
+      $(document).ready(function() {
+            // Initialize the Google Map
+
+            // Set up the Appraisal toggle buttons
+            $('#humanAppraisal').on('click', function (e) {
+                  document.getElementById("nodeAppraisalPlot").innerHTML = 
+                  '<img alt="Human Appraisal Plot" src="img/human_appraisals/Human_Appraisal_node' 
+                  + zeroPad(node,3) + '.png">';
+            });
+            $('#machineAppraisal').on('click', function (e) {
+                  document.getElementById("nodeAppraisalPlot").innerHTML = 
+                  '<img alt="Human Appraisal Plot" src="img/appraisals/ESN_Appraisal_Density_node' + 
+                  zeroPad(node,3) + '.png">';
+            });
+
+
+      });
+
+      function changeSomething () {
+            var chart = c3.generate({
+                  bindto: "#pieChart",
+                  data: {
+                        // iris data from R
+                        columns: [
+                        ['Chaotic', 10],
+                        ['Lively' , 10],
+                        ['Boring' , 10],
+                        ['Calm'   , 10],
+                        ],
+                        type : 'pie',
+                  }
+            });
+      }
+   </script>
 </body>
 </html>
